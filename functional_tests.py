9666895c41e46bb1d5c479f697261079925a9d25
George Cai <georgemail608@gmail.com>
2017-06-12 13:22:48 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -38,10 +38,11 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTure(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
-        
+
         # 页面中有显示了一个文本框，可以输入其他的待办事项
         # 她输入了”Use peacock feathers to make a fly"
         # 伊迪丝做事很有调理
