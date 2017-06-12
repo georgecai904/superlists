5126b843f0c9bbdc8586e9c13bb5a58c4a2eedd1
George Cai <georgemail608@gmail.com>
2017-06-12 17:07:36 +0800

    refactor check rows

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-
+        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
@@ -49,12 +49,20 @@ class NewVisitorTest(unittest.TestCase):
         # 页面中有显示了一个文本框，可以输入其他的待办事项
         # 她输入了”Use peacock feathers to make a fly"
         # 伊迪丝做事很有调理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly",
+            [row.text for row in rows]
+        )
 
         # 伊迪丝想知道这个网站是否会记住她的清单
-
+        self.fail('Finish the test')
         # 她看到网站为她声称了一个唯一的URL
         # 而且网页中有一些文字解说了这个功能
 
