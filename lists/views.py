5fc9866681663dd95b52605eb2bc1b88060f6738
George Cai <georgemail608@gmail.com>
2017-06-13 10:45:55 +0800

    new list view and test case

--- lists/views.py
+++ lists/views.py
@@ -4,14 +4,12 @@ from .models import Item
 
 # Create your views here.
 def home_page(request):
-    # return HttpResponse("<html><title>To-Do lists</title></html>")
-    if request.method == "POST":
-        # return HttpResponse(request.POST['item_text'])
-        new_item_text = request.POST.get('item_text')
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'lists/home.html')
 
 def view_lists(request):
     items = Item.objects.all()
     return render(request, 'lists/list.html', {"items": items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
