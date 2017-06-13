b3b718b2702cac1d323c3169cbb3b34e3d597924
George Cai <georgemail608@gmail.com>
2017-06-13 12:51:23 +0800

    new URL + view for adding to existing lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 import lists.views
 
 urlpatterns = [
     url(r'^$', lists.views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', lists.views.view_lists,
-        name='view_list'),
-    url(r'^lists/new$', lists.views.new_list, name='new_list')
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', admin.site.urls),
 ]
