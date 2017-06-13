a70a23f9102fb036f85870f7da898a8e0c7b8565
George Cai <georgemail608@gmail.com>
2017-06-13 14:38:40 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path='/driver/geckodriver')
@@ -91,7 +91,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("Buy milk", page_text)
 
         #两人都很满意，去睡觉了
-        self.fail("Finish the test")
 
     def test_layout_and_styling(self):
         # 伊迪丝访问网页
