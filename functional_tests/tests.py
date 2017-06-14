e7faf9c89be4c711c363fce00567bd7d4f5699ac
George Cai <georgemail608@gmail.com>
2017-06-14 16:28:13 +0800

    Add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -49,7 +49,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -60,6 +60,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
         edith_list_url = self.browser.current_url
